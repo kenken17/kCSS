342643dd42796c99eb150bc2bdbdd5ea3a777c49
ken <ken@mindnaked.com>
2018-07-18 22:41:43 +0800

    Make container e2e using calcuated width/height.

--- tests/layout/container.js
+++ tests/layout/container.js
@@ -1,3 +1,7 @@
+import { mobile, tablet, desktop, bigScreen } from "../breakpoints";
+
+let mainContainerHeight;
+
 module.exports = {
     tags: ["container"],
     "Should have all the containers and children"(client) {
@@ -21,11 +25,16 @@ module.exports = {
             .resizeWindow(mobile.width, mobile.height)
             .waitForElementVisible(".main-container");
 
+        // First set the window height
+        client.getElementSize(".main-container", function(result) {
+            mainContainerHeight = result.value.height;
+        });
+
         client.getElementSize(".child-a", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 400);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, mobile.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-a", function(result) {
@@ -47,15 +56,15 @@ module.exports = {
         client.getElementSize(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 400);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, mobile.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 100);
+            this.assert.equal(result.value.y, mainContainerHeight / 4);
         });
 
         client.end();
@@ -70,15 +79,15 @@ module.exports = {
         client.getElementSize(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 400);
-            this.assert.equal(result.value.height, 200);
+            this.assert.equal(result.value.width, mobile.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 2);
         });
 
         client.getLocation(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 200);
+            this.assert.equal(result.value.y, mainContainerHeight / 2);
         });
 
         client.end();
@@ -93,15 +102,15 @@ module.exports = {
         client.getElementSize(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 400);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, mobile.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 200);
+            this.assert.equal(result.value.y, mainContainerHeight / 2);
         });
 
         client.end();
@@ -116,15 +125,15 @@ module.exports = {
         client.getElementSize(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 400);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, mobile.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 300);
+            this.assert.equal(result.value.y, mainContainerHeight / 4 * 3);
         });
 
         client.end();
@@ -133,14 +142,19 @@ module.exports = {
     "Tablet: Should see child-a at top"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
-            .resizeWindow(table.width, tablet.height)
+            .resizeWindow(tablet.width, tablet.height)
             .waitForElementVisible(".main-container");
+
+        // First set the window height
+        client.getElementSize(".main-container", function(result) {
+            mainContainerHeight = result.value.height;
+        });
 
         client.getElementSize(".child-a", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, tablet.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-a", function(result) {
@@ -156,20 +170,20 @@ module.exports = {
     "Tablet: Should see child-b as the second cell in a row"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
-            .resizeWindow(table.width, tablet.height)
+            .resizeWindow(tablet.width, tablet.height)
             .waitForElementVisible(".main-container");
 
         client.getElementSize(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, tablet.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 200);
+            this.assert.equal(result.value.x, tablet.width / 3);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -179,20 +193,20 @@ module.exports = {
     "Tablet: Should see child-c as the third cell in the row"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
-            .resizeWindow(table.width, tablet.height)
+            .resizeWindow(tablet.width, tablet.height)
             .waitForElementVisible(".main-container");
 
         client.getElementSize(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, tablet.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 400);
+            this.assert.equal(result.value.x, tablet.width / 3 * 2);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -202,20 +216,20 @@ module.exports = {
     "Tablet: Should see child-c-1 same as child-c but is half (100px)"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
-            .resizeWindow(table.width, tablet.height)
+            .resizeWindow(tablet.width, tablet.height)
             .waitForElementVisible(".main-container");
 
         client.getElementSize(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 100);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, tablet.width / 3 / 2);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 400);
+            this.assert.equal(result.value.x, tablet.width / 3 * 2);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -225,20 +239,20 @@ module.exports = {
     "Tablet: Should see child-c-2 as the last cell on the right"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
-            .resizeWindow(table.width, tablet.height)
+            .resizeWindow(tablet.width, tablet.height)
             .waitForElementVisible(".main-container");
 
         client.getElementSize(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 100);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, tablet.width / 3 / 2);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 500);
+            this.assert.equal(result.value.x, tablet.width / 3 / 2 * 5);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -251,11 +265,16 @@ module.exports = {
             .resizeWindow(desktop.width, desktop.height)
             .waitForElementVisible(".main-container");
 
+        // First set the window height
+        client.getElementSize(".main-container", function(result) {
+            mainContainerHeight = result.value.height;
+        });
+
         client.getElementSize(".child-a", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 1200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, desktop.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-a", function(result) {
@@ -277,15 +296,15 @@ module.exports = {
         client.getElementSize(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 1200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, desktop.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 100);
+            this.assert.equal(result.value.y, mainContainerHeight / 4);
         });
 
         client.end();
@@ -300,15 +319,15 @@ module.exports = {
         client.getElementSize(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 1200);
-            this.assert.equal(result.value.height, 200);
+            this.assert.equal(result.value.width, desktop.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 2);
         });
 
         client.getLocation(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 200);
+            this.assert.equal(result.value.y, mainContainerHeight / 2);
         });
 
         client.end();
@@ -323,15 +342,15 @@ module.exports = {
         client.getElementSize(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 1200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, desktop.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 200);
+            this.assert.equal(result.value.y, mainContainerHeight / 2);
         });
 
         client.end();
@@ -346,31 +365,36 @@ module.exports = {
         client.getElementSize(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 1200);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, desktop.width);
+            this.assert.equal(result.value.height, mainContainerHeight / 4);
         });
 
         client.getLocation(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
             this.assert.equal(result.value.x, 0);
-            this.assert.equal(result.value.y, 300);
+            this.assert.equal(result.value.y, mainContainerHeight / 4 * 3);
         });
 
         client.end();
     },
 
-    "Big Screen: Should see child-a at top"(client) {
+    "Big Screen: Should see child-a on the left"(client) {
         client
             .url("http://localhost:8080/examples/layout/container.html")
             .resizeWindow(bigScreen.width, bigScreen.height)
             .waitForElementVisible(".main-container");
 
+        // First set the window height
+        client.getElementSize(".main-container", function(result) {
+            mainContainerHeight = result.value.height;
+        });
+
         client.getElementSize(".child-a", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 600);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, bigScreen.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-a", function(result) {
@@ -392,14 +416,14 @@ module.exports = {
         client.getElementSize(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 600);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, bigScreen.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-b", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 600);
+            this.assert.equal(result.value.x, bigScreen.width / 3);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -415,14 +439,14 @@ module.exports = {
         client.getElementSize(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 600);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, bigScreen.width / 3);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 1200);
+            this.assert.equal(result.value.x, bigScreen.width / 3 * 2);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -440,14 +464,14 @@ module.exports = {
         client.getElementSize(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 300);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, bigScreen.width / 3 / 2);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c-1", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 1200);
+            this.assert.equal(result.value.x, bigScreen.width / 3 * 2);
             this.assert.equal(result.value.y, 0);
         });
 
@@ -463,14 +487,14 @@ module.exports = {
         client.getElementSize(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.width, 300);
-            this.assert.equal(result.value.height, 100);
+            this.assert.equal(result.value.width, bigScreen.width / 3 / 2);
+            this.assert.equal(result.value.height, mainContainerHeight);
         });
 
         client.getLocation(".child-c-2", function(result) {
             this.assert.equal(typeof result, "object");
             this.assert.equal(result.status, 0);
-            this.assert.equal(result.value.x, 1500);
+            this.assert.equal(result.value.x, bigScreen.width / 6 * 5);
             this.assert.equal(result.value.y, 0);
         });
 
